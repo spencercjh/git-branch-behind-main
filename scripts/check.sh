92c659f3db53bf05b1b5d16e4544ea79a23cd311
hwk2077 <huang_wenkang@outlook.com>
2023-02-20 18:09:48 +0800

    debug: git rev-list

--- scripts/check.sh
+++ scripts/check.sh
@@ -15,6 +15,9 @@ git rev-list @
 echo $REFERENCE_BRANCH
 git rev-list $REFERENCE_BRANCH
 
+echo $REFERENCE_BRANCH
+git rev-list origin/$REFERENCE_BRANCH
+
 echo $CURRENT_FEATURE
 git rev-list $CURRENT_FEATURE
 
